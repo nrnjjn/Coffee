c7d2b31e7c622b90a5c99c83ae40cdc7ba53d4f5
Niranjan <niranjanbabu6677@gmail.com>
2024-06-25 21:15:30 +0530

    port

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use('/user',userRouter)
 
 // app.listen(4000)
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.listen(port,()=>{
   console.log(`Server is running on port: ${port}`)
